922eff80cd3431ad025cce250db388dc9448158f
kirillgt33 <kirillgt33@gmail.com>
2026-05-21 21:08:52 +0500

    Fix terraform manifests zone and subnet parameters

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,34 @@
-# 1. Создаем изолированную виртуальную сеть (VPC)
-resource "yandex_vpc_network" "music_net" {
-  name = "vxga-music-network"
+# Описываем диск для операционной системы
+resource "yandex_compute_disk" "boot_disk" {
+  name     = "music-web-boot-disk"
+  type     = "network-hdd"
+  zone     = "kz-central1-a"
+  size     = 15
+  image_id = "fd80le8b88let16p969t" # ID образа Ubuntu 22.04 LTS
 }
 
-# 2. Создаем подсеть в зоне ru-central1-a
-resource "yandex_vpc_subnet" "music_subnet" {
-  name           = "vxga-music-subnet"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.music_net.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
-
-# 3. Поднимаем сам облачный сервер
+# Описываем саму виртуальную машину
 resource "yandex_compute_instance" "music_server" {
   name        = "vxga-music-vm"
-  platform_id = "standard-v3" # Третье поколение процессоров (Intel Ice Lake)
+  platform_id = "standard-v3" # Третье поколение процессоров Intel
+  zone        = "kz-central1-a"
 
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 20 # Экономичный режим: процессор работает на 20% мощности, для Flask этого выше крыши
+    cores  = 2
+    memory = 2
   }
 
   boot_disk {
-    initialize_params {
-      image_id = "fd80bm0g9v98066v9694" # Официальный чистый образ Ubuntu 22.04 LTS
-      size     = 15                     # Диск на 15 ГБ (хватит для системы и докера)
-    }
+    disk_id = yandex_compute_disk.boot_disk.id
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.music_subnet.id
-    nat       = true # Включает внешний публичный IP-адрес, чтобы сайт был виден в интернете
+    subnet_id = "ad1oahu1bbg5kc6hssej"
+    nat       = true # Включаем публичный IP, чтобы сайт работал в интернетах
   }
 
   metadata = {
-    # Автоматически прокидываем твой публичный SSH-ключ для пользователя ubuntu
-    # Чтобы ты и Ansible могли заходить на сервер без паролей
+    # Передаем публичный SSH-ключ для безопасного доступа
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
-}
-
-# Выводим IP-адрес сервера в консоль после успешного деплоя
-output "external_ip" {
-  value       = yandex_compute_instance.music_server.network_interface.0.nat_ip_address
-  description = "Публичный IP-адрес музыкального сервера"
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,12 +4,10 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
 }
 
 provider "yandex" {
-  # Тут пока оставляем пустым, мы авторизуемся через консольную утилиту чуть позже
   cloud_id  = "ao7odmh34pr4sq575v1a"
   folder_id = "ao76ri81fetlanol6fe3"
-  zone      = "ru-central1-a" # Самая популярная и дешевая зона
+  zone      = "kz-central1-a"
 }
